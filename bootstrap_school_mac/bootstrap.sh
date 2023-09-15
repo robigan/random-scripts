f012a526d618e2ba5d0a0de9d0c780c63e9a1950
robigan <35210888+robigan@users.noreply.github.com>
2023-09-15 11:28:25 +0200

    Update bootstrap.sh

--- bootstrap_school_mac/bootstrap.sh
+++ bootstrap_school_mac/bootstrap.sh
@@ -23,38 +23,53 @@ else
 fi
 
 # Homebrew install
-echo -n "Do you want to install Homebrew? (y/n) "
+echo -n "Do you want to install Xcode Command Line Utilities? (y/n) "
 read install_brew
 
 if [[ $install_brew =~ ^[Yy]$ ]]; then
   # Prevent sleep
   caffeinate -i &
 
-  if xcode-select --install; then
-    echo "Xcode command line tools installed successfully"
+  trap 'echo "Caught INT signal, cleaning up..."; kill $!' INT
 
-    # Install Homebrew
-    if git clone --depth=5 https://github.com/Homebrew/brew ~/homebrew; then
-      echo "Homebrew cloned"
-
-      # Additional Homebrew setup  
-      eval "$(~/homebrew/bin/brew shellenv)"
-      brew update --force --quiet
-      chmod -R go-w "$(brew --prefix)/share/zsh"
-
-      echo 'eval "\$(${HOMEBREW_PREFIX}/bin/brew shellenv)"' >> ~/.zshrc
-      echo 'export HOMEBREW_CASK_OPTS="--appdir=$HOME/Applications"' >> ~/.zshrc
-
-      echo "Homebrew setup complete"
+  {
+    # Install Xcode tools
+    if xcode-select --install; then
+      echo "Xcode command line tools installed successfully"
     else
-      echo "Failed to install Homebrew"
+      echo "Failed to install Xcode command line tools"
     fi
-  else
-    echo "Failed to install Xcode command line tools"
-  fi
+
+    echo -n "Also install Homebrew? (y/n)"
+    read install_brew
+
+    if [[ $install_brew =~ ^[Yy]$ ]]; then
+      # Install Homebrew
+      {
+        git clone --depth=5 https://github.com/Homebrew/brew ~/homebrew
+        echo "Homebrew cloned"
+
+        # Additional Homebrew setup
+        eval "$(~/homebrew/bin/brew shellenv)"
+        brew update --force --quiet
+        chmod -R go-w "$(brew --prefix)/share/zsh"
+
+        mkdir -p $HOME/Applications
+        echo "$HOME/Applications created"
+        echo "eval \"\$(${HOMEBREW_PREFIX}/bin/brew shellenv)\"" >> ~/.zshrc
+        echo 'export HOMEBREW_CASK_OPTS="--appdir=$HOME/Applications"' >> ~/.zshrc
+
+        echo "Homebrew setup complete"
+      } || {
+        echo "Failed to install Homebrew"
+      }
+    fi
+  } || {
+    echo "Installation exited with non zero status code"
+  }
 
   # Allow sleep
   kill $!
 else
-  echo "Skipping Homebrew installation"
+  echo "Skipping Xcode/Homebrew installation"
 fi
